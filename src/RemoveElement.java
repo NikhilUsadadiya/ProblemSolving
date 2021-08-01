c93a2c9fe188287aef72fe97b0044dc7da0cedda
Nikhil <nikhil.usadadiya@gmail.com>
2021-08-01 23:29:43 +0530

    Update by Nikhil.... Added code for RemoveElement....

---
+++ src/RemoveElement.java
@@ -0,0 +1,27 @@
+
+public class RemoveElement {
+
+    static int i = 0;
+    static int[] nums1 = {3, 2, 2, 3};
+    static int value = 3;
+
+    public static void main(String[] args) {
+        removeElement(nums1, value);
+    }
+
+
+    private static void removeElement(int[] nums1, int value) {
+        for (int j = 0; j < nums1.length; j++) {
+            if (nums1[j] != value) {
+                nums1[i] = nums1[j];
+                i++;
+            }
+        }
+
+        /*for (int k=0; k<nums1.length; k++) {
+            System.out.println("k= " + nums1[k]);
+        }*/
+        System.out.println("i:" + i);
+    }
+
+}
